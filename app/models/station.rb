bf8f48e2b5d2f72e650ba4127bcb3266a9ce29a3
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-06 10:20:32 -0700

    most of the information printed to page need to add distance and figure out why zip code returns california

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,8 +1,13 @@
 class Station
+  attr_reader :name,
+              :address,
+              :fuel_types,
+              :access_times
 
   def initialize(stations)
-    require "pry"; binding.pry
     @name = stations[:station_name]
     @address = stations[:street_address]
+    @fuel_types = stations[:fuel_type_code]
+    @access_times = stations[:access_days_time]
   end
 end
--- spec/features/user_can_search_using_zip_spec.rb
+++ spec/features/user_can_search_using_zip_spec.rb
@@ -4,14 +4,14 @@ describe 'User can search using zip code' do
   it 'shows stations on /search' do
     facade = SearchFacade.new("80203")
     facade.stations
+
     visit '/'
 
     fill_in "q", with: 80203
     click_on "Locate"
 
     expect(current_path).to eq(search_path)
-    expect(station.count).to eq(10)
-
+    
 
   end
 end
