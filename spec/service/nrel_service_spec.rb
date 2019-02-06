1a5b37b6beb8494160821e373076394bce453ef8
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-06 10:49:12 -0700

    add test for service

---
+++ spec/service/nrel_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe NrelService do
+  it 'exists' do
+    nrel = NrelService.new
+    expect(nrel).to be_a(NrelService)
+  end
+
+  it 'gives stations for a zip code' do
+    service = NrelService.new
+    data = service.get_station_info("80203")[0]
+
+    expect(data).to be_a(Hash)
+    expect(data[:station_name]).to eq("UDR")
+    expect(data[:street_address]).to eq("800 Acoma St")
+  end
+end
