a082ce4d2e6b0ce05f86ba5df830b12e8e62e75c
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-06 10:24:59 -0700

    removed extra code from test

--- spec/features/user_can_search_using_zip_spec.rb
+++ spec/features/user_can_search_using_zip_spec.rb
@@ -2,16 +2,12 @@ require 'rails_helper'
 
 describe 'User can search using zip code' do
   it 'shows stations on /search' do
-    facade = SearchFacade.new("80203")
-    facade.stations
-
     visit '/'
 
     fill_in "q", with: 80203
     click_on "Locate"
 
     expect(current_path).to eq(search_path)
-    
 
   end
 end
