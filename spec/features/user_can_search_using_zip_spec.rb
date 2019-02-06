4d6c664b44bb1fd6e7bdfe9bdd0410418f21055a
Nick Lindeberg <lindebergnick@gmail.com>
2019-02-06 09:18:08 -0700

    initial test setup will add more once service is built

---
+++ spec/features/user_can_search_using_zip_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'User can search using zip code' do
+  it 'shows stations on /search' do
+    visit '/'
+
+    fill_in "q", with: 80203
+    click_on "Locate"
+
+    expect(current_path).to eq(search_path)
+
+  end
+end
+
+
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203 (Note: Use the existing search form)
+# And I click "Locate"
+# Then I should be on page "/search"
+
+
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
